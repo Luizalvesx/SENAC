704f37681569b011a85957ebef63f2112b03ce45
Luizalvesx <neto.infox@gmail.com>
2023-09-25 16:24:55 -0300

    Atividade somanumpares

---
+++ somanumpares.py
@@ -0,0 +1,9 @@
+def fatorial(numero):
+    if numero == 0:
+        return 1
+    else:
+        return numero * fatorial(numero - 1)
+
+n = int(input("Digite um número inteiro para calcular o fatorial: "))
+resultado = fatorial(n)
+print(f"O fatorial de {n} é {resultado}")
